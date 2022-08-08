2a3b9e560356f33ee83e1c31c6503125d2b6392c
manjunath2019 <46236223+manjunath2019@users.noreply.github.com>
2022-08-08 12:38:42 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,10 @@
+resource "aws_vpc" "development-vpc" {
+    cidr_block = var.cidr_blocks[0].cidr_block
+    tags = {
+        Name = "${lower(var.vendor)}-${lower(var.environment)}-vpc"
+    }
+}
+data "aws_vpc" "existing_vpc" {
+    #"query existing resources"
+    id = aws_vpc.development-vpc.id
+}
